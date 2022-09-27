f8cf295db2299434566bdbaed68453f2ea00e6d9
jpabloallin <jpac.647@gmail.com>
2022-09-27 11:05:01 -0500

    prettier code

--- src/common/helpers/headers.helper.ts
+++ src/common/helpers/headers.helper.ts
@@ -1,11 +1,11 @@
-import { v4 as uuid } from "uuid";
-import { ConfigService } from "@nestjs/config";
+import { v4 as uuid } from 'uuid';
+import { ConfigService } from '@nestjs/config';
 
 export const getHeaders = (configService: ConfigService) => {
-    return {
-      apikey: configService.get('APIKEY'),
-      Accept: 'application/vnd.mambu.v2+json',
-      'Content-Type': 'application/json',
-      idempotencyKey: uuid(),
-    };
+  return {
+    apikey: configService.get('APIKEY'),
+    Accept: 'application/vnd.mambu.v2+json',
+    'Content-Type': 'application/json',
+    idempotencyKey: uuid(),
   };
+};
